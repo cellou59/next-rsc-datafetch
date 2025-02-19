d82091acdecf14d226a097f6d2fe8edbdb102d72
cellouRetailShake <cellu@retailshake.com>
2025-02-19 09:38:46 +0100

    refactor: replace fetchPosts function with direct database call for improved performance

--- src/app/exercises/post/page.exercise.tsx
+++ src/app/exercises/post/page.exercise.tsx
@@ -1,13 +1,16 @@
+import {getPosts} from '@/db/sgbd'
 import {Post} from '@/lib/type'
-import Posts from './posts'
 
-const fetchPosts = async (): Promise<Post[]> => {
-  const response = await fetch('http://localhost:3000/exercises/api/posts')
-  return await response.json()
-}
-
-const Page = () => {
-  return <Posts fetchPosts={fetchPosts()} />
+const Page = async () => {
+  const posts = await getPosts()
+  return (
+    <div className="mx-auto max-w-4xl p-6 text-lg">
+      <h1 className="mb-4 text-center text-3xl font-bold"> Fetch Posts</h1>
+      <ul className="list-disc p-4 pl-4">
+        {posts?.map((post: Post) => <li key={post.title}>{post.title}</li>)}
+      </ul>
+    </div>
+  )
 }
 
 export default Page
