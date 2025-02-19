809a5a492a77734854b1578e41d3f030de9af168
cellouRetailShake <cellu@retailshake.com>
2025-02-19 07:16:39 +0100

    refactor: simplify post fetching by removing useEffect and useState, utilizing use() instead

--- src/app/exercises/post/page.exercise.tsx
+++ src/app/exercises/post/page.exercise.tsx
@@ -1,40 +1,15 @@
 'use client'
 import {Post} from '@/lib/type'
-// ⛏️ Supprime `useEffect` et `useState` nous n'en avons plus besoin
-import {useEffect, useState} from 'react'
-// 🐶 À la place, importe `use` from "react"
+import {use} from 'react'
 
-// 🐶 Crée une fonction `fetchPosts`
-// 🤖
-// const fetchPosts = async () => {
-//   const response = await fetch('http://localhost:3000/exercises/api/posts')
-//   const data = await response.json()
-//   return data as Post[]
-// }
+const fetchPosts = async () => {
+  const response = await fetch('http://localhost:3000/exercises/api/posts')
+  const data = await response.json()
+  return data as Post[]
+}
 
 const Page = () => {
-  // ⛏️ Supprime ce state
-  const [posts, setPosts] = useState<Post[]>([])
-
-  // ⛏️ Supprime le `useEffect`
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await fetch(
-          'http://localhost:3000/exercises/api/posts'
-        )
-        const data = await response.json()
-        setPosts(data as Post[])
-      } catch (error) {
-        console.error('Error fetching posts:', error)
-      }
-    }
-    fetchPosts()
-  }, [])
-
-  // 🐶 Appelle la fonction `fetchPosts()` dans `use()` et affecte le résultat dans "posts"
-  // 🐶 logue posts : console.log(posts)
-  // Constate que nous sommes toujours en train de fetch les posts dans un composants clients
+  const posts = use<Post[]>(fetchPosts())
   return (
     <div className="mx-auto max-w-4xl p-6 text-lg">
       <h1 className="mb-4 text-center text-3xl font-bold"> Fetch Posts</h1>
