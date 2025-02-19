89ae3fdc74345232e737e3d129bce7aa97270cb1
cellouRetailShake <cellu@retailshake.com>
2025-02-19 05:56:16 +0100

    update fetch URL for posts to local server

--- src/app/exercises/post/page.exercise.tsx
+++ src/app/exercises/post/page.exercise.tsx
@@ -8,9 +8,7 @@ const Page = () => {
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const response = await fetch(
-          'https://jsonplaceholder.typicode.com/posts'
-        )
+        const response = await fetch('http://localhost:4000/posts/')
         const data = await response.json()
         setPosts(data as Post[])
       } catch (error) {
