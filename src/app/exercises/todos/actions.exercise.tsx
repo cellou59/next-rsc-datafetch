80e1364ec15596030f156b03c56ccdffa97f917b
cellouRetailShake <cellu@retailshake.com>
2025-02-19 20:42:43 +0100

    refactor: improve error handling in addTodo action and update imports

--- src/app/exercises/todos/actions.exercise.tsx
+++ src/app/exercises/todos/actions.exercise.tsx
@@ -1,12 +1,18 @@
-// 🐶 Ajoute la directive `use server` pour spécifier que nous faisons des server actions
-// 🤖 'use server'
+'use server'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import {addTodo as addTodoDao} from '@/db/sgbd'
 import {AddTodo} from '@/lib/type'
 
 export const addTodo = async (todo: AddTodo) => {
   console.log('add todo action', todo)
+  try {
+    addTodoDao(todo)
+  } catch (error) {
+    if (error) {
+      console.error(error)
+    }
+    throw new Error(`Oups : ${error}`)
+  }
   // 🐶 Appelle `addTodoDao(todo)` dans un `try catch`,
   // En effet, il faut pouvoir gérer les erreurs d'insersion en BDD.
   // Fais un `console.error` en cas d'erreur
